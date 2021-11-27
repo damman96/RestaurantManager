89c7adde5b764d88a3a879f8c70785388909aca5
damian <damianwrobel35@gmail.com>
2021-11-27 18:10:17 +0100

    Initial commit

---
+++ src/test/java/restaurantmanager/RestaurantManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurantmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagerApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
